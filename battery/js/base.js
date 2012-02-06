b44447a6f059141ab7c8ed65356e8aaf202c0205
Robert Nyman <robert@robertnyman.com>
2012-02-06 14:03:00 +0100

    Added console logging and removed old code

--- battery/js/base.js
+++ battery/js/base.js
@@ -6,11 +6,11 @@
         batteryCharged = document.getElementById("battery-charged"),
         batteryDischarged = document.getElementById("battery-discharged");
 
-alert(battery)
+    console.log(battery);
 
     if (battery) {
         function setStatus () {
-            // alert("set status");
+            console.log(battery);
             batteryLevel.innerHTML = battery.level * 100 + "%";
             chargingStatus.innerHTML = (battery.charging)? "" : "not ";
             batteryCharged.innerHTML = battery.chargingTime;
@@ -28,23 +28,3 @@ alert(battery)
         batterySupported.innerHTML = "Battery API not supported on your device/computer";
     } 
 })();
-
-/*    b.addEventListener("chargingchange", function() {
-        if (b.charging) {
-            alert("Charging!" + "\nLevel: " + b.level * 100 + "%");
-            window.location.reload(true);
-        } else {
-            alert("Unplugged!" + "\nLevel: " + b.level * 100 + "%");
-            window.location.reload(true);
-        }
-    });
-
-    b.addEventListener("levelchange", function() {
-        window.location.reload(true);			
-    });
-
-    if (b.charging) {
-        document.write("<h2>Shows charging state, charge level, and charging time for device battery.</h2>" + "Charging! <p>Level: " + b.level * 100 + "%</p>" + "<p>Charging Time: " + b.chargingTime + " seconds until the battery is fully charged.</p>");
-    } else {
-        document.write("<h2>Shows discharging state, charge level and discharging time for device battery.</h2>" + "Discharging! <p>Level: " + b.level * 100 + "%</p>" + "<p>Discharging Time: " + b.dischargingTime + " seconds until the battery is fully discharged.</p>")
-};*/
