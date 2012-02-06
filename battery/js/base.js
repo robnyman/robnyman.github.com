14ae7e88ad037267ec15f142c1d75049dd8f42db
Robert Nyman <robert@robertnyman.com>
2012-02-06 12:33:52 +0100

    Testing Battery API

---
+++ battery/js/base.js
@@ -0,0 +1,44 @@
+(function () {
+    var battery = navigator.mozBattery, 
+        batterySupported = document.getElementById("battery-supported"),
+        batteryLevel = document.getElementById("battery-level"),
+        chargingStatus = document.getElementById("charging-status"),
+        batteryCharged = document.getElementById("battery-charged"),
+        batteryDischarged = document.getElementById("battery-discharged");
+
+alert(battery)
+
+    if (battery) {
+        function setStatus () {
+            batteryLevel.innerHTML = b.level;
+            chargingStatus.innerHTML = b.charging;
+            batteryCharged.innerHTML = b.chargingTime;
+            batteryDischarged.innerHTML = b.dischargingTime;
+        }
+        battery.addEventListener("chargingchange", setStatus, false);
+        battery.addEventListener("levelchange", setStatus, false);
+    }
+    else {
+        batterySupported.innerHTML = "Battery API not supported on your device/computer";
+    } 
+})();
+
+/*    b.addEventListener("chargingchange", function() {
+        if (b.charging) {
+            alert("Charging!" + "\nLevel: " + b.level * 100 + "%");
+            window.location.reload(true);
+        } else {
+            alert("Unplugged!" + "\nLevel: " + b.level * 100 + "%");
+            window.location.reload(true);
+        }
+    });
+
+    b.addEventListener("levelchange", function() {
+        window.location.reload(true);			
+    });
+
+    if (b.charging) {
+        document.write("<h2>Shows charging state, charge level, and charging time for device battery.</h2>" + "Charging! <p>Level: " + b.level * 100 + "%</p>" + "<p>Charging Time: " + b.chargingTime + " seconds until the battery is fully charged.</p>");
+    } else {
+        document.write("<h2>Shows discharging state, charge level and discharging time for device battery.</h2>" + "Discharging! <p>Level: " + b.level * 100 + "%</p>" + "<p>Discharging Time: " + b.dischargingTime + " seconds until the battery is fully discharged.</p>")
+};*/
