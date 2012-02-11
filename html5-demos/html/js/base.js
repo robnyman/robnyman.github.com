fdd7afb5f59eae76cb7307c9a88a91ca7511a4b4
Robert Nyman <robert@robertnyman.com>
2012-02-11 23:51:07 +0100

    Added demos for classList and custom data attributes

--- html5-demos/html/js/base.js
+++ html5-demos/html/js/base.js
@@ -1,3 +1,22 @@
 (function () {
-    alert("Hello");
+    var elm = document.getElementById("classlist-demo");
+    elm.classList.add("boxy");
+    elm.classList.add("pretty");
+    elm.classList.remove("pretty");
+    elm.classList.toggle("pretty");
+    elm.innerHTML += "<br>Contains pretty: " + elm.classList.contains("pretty");
+    elm.innerHTML += "<br>" + elm.classList.toString();
+
+
+    var customAttributes = document.getElementById("custom-attributes"),
+        attributes
+        dataset = [];
+    attributes = "<br>data-type: " + customAttributes.getAttribute("data-type");
+    attributes += "<br>data-value: " + customAttributes.getAttribute("data-value");
+    attributes += "<br>dataset: ";
+    for (var i in customAttributes.dataset) {
+        dataset.push(i + ": " + customAttributes.dataset[i]);
+    }
+    attributes += dataset.join(", ");
+    customAttributes.innerHTML += attributes;
 })();
