f5294bb81ae4b89d574af00abec5fdc43fe3efcf
Robert Nyman <robert@robertnyman.com>
2012-06-05 10:23:15 +0200

    Updated to using addListener

--- matchmedia/js/base.js
+++ matchmedia/js/base.js
@@ -2,17 +2,43 @@
     var matchMediaSupported = document.querySelector("#matchmedia-supported"),
         width600 = document.querySelector("#width-600"),
         height500 = document.querySelector("#height-500"),
-        portraitOrientation = document.querySelector("#portrait-orientation");
+        portraitOrientation = document.querySelector("#portrait-orientation"),
+        width600Check,
+        height500Check,
+        portraitOrientationCheck;
+
     if (window.matchMedia) {
         matchMediaSupported.innerHTML = "supported";
+
+        // Establishing media check
+        width600Check = window.matchMedia("(min-width: 600px)"),
+        height500Check = window.matchMedia("(min-height: 500px)"),
+        portraitOrientationCheck = window.matchMedia("(orientation: portrait)");
+       
+        // Add listeners for detecting changes
+        width600Check.addListener(setValues);
+        height500Check.addListener(setValues);
+        portraitOrientationCheck.addListener(setValues);
+    }
+
+    function setWidthValue (mediaQueryList) {
+        width600.innerHTML = mediaQueryList.matches;
+    }
+
+    function setHeightValue (mediaQueryList) {
+        height500.innerHTML = mediaQueryList.matches;
+    }
+
+    function setOrientationValue (mediaQueryList) {
+        portraitOrientation.innerHTML = mediaQueryList.matches;
     }
 
     function setValues () {
-        width600.innerHTML = (window.matchMedia("(min-width: 600px)")).matches;
-        height500.innerHTML = (window.matchMedia("(min-height: 600px)")).matches;
-        portraitOrientation.innerHTML = (window.matchMedia("(orientation: portrait)")).matches;
+        width600.innerHTML = width600Check.matches;
+        height500.innerHTML = height500Check.matches;
+        portraitOrientation.innerHTML = portraitOrientationCheck.matches;
     }
-    window.addEventListener("resize", setValues, false);
+
     window.addEventListener("DOMContentLoaded", setValues, false);
 })();
 
