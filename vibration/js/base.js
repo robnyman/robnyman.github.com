dafd656138a54281855d91878bf94d96221e0972
Robert Nyman <robert@robertnyman.com>
2013-01-03 07:22:11 +0100

    Removed prefix

--- vibration/js/base.js
+++ vibration/js/base.js
@@ -1,19 +1,19 @@
 (function () {
     document.querySelector("#vibrate-one-second").addEventListener("click", function () {
-        navigator.mozVibrate(1000);
+        navigator.vibrate(1000);
     }, false);
 
     document.querySelector("#vibrate-twice").addEventListener("click", function () {
-        navigator.mozVibrate([200, 100, 200, 100]);
+        navigator.vibrate([200, 100, 200, 100]);
     }, false);
 
 
     document.querySelector("#vibrate-long-time").addEventListener("click", function () {
-        navigator.mozVibrate(5000);
+        navigator.vibrate(5000);
     }, false);
 
     document.querySelector("#vibrate-off").addEventListener("click", function () {
-        navigator.mozVibrate(0);
+        navigator.vibrate(0);
     }, false);
 })();
 
