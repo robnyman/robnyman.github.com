bcdf1b3d13f3814ae56ac0d245c7e16b973ac53d
Chiamaka Nwolisa <chiamakanwolisa@gmail.com>
2016-12-15 02:04:01 +0100

    Refactor Battery API code
    The JS code written to demo the battery API was written 4 years ago and a lot of things have changed.
    The battery API makes use of promises and I changed the old code to reflect the new changes. ES6 arrow
    function feature is also included. Also the battery API now works in Chrome not only Firefox

--- battery/js/base.js
+++ battery/js/base.js
@@ -1,32 +1,38 @@
 (function () {
-    var battery = navigator.battery, 
-        batterySupported = document.getElementById("battery-supported"),
+    let batterySupported = document.getElementById("battery-supported"),
         batteryLevel = document.getElementById("battery-level"),
         chargingStatus = document.getElementById("charging-status"),
         batteryCharged = document.getElementById("battery-charged"),
         batteryDischarged = document.getElementById("battery-discharged");
 
-    console.log(battery);
+    let success = function (battery) {
+        if (battery) {
+            function setStatus () {
+                console.log("Set status");
+                batteryLevel.innerHTML = Math.round(battery.level * 100) + "%";
+                chargingStatus.innerHTML = (battery.charging)? "" : "not ";
+                batteryCharged.innerHTML = (battery.chargingTime == "Infinity")? "Infinity" : parseInt(battery.chargingTime / 60, 10);
+                batteryDischarged.innerHTML = (battery.dischargingTime == "Infinity")? "Infinity" : parseInt(battery.dischargingTime / 60, 10);
+            }
 
-    if (battery) {
-        function setStatus () {
-            console.log("Set status");
-            batteryLevel.innerHTML = Math.round(battery.level * 100) + "%";
-            chargingStatus.innerHTML = (battery.charging)? "" : "not ";
-            batteryCharged.innerHTML = (battery.chargingTime == "Infinity")? "Infinity" : parseInt(battery.chargingTime / 60, 10);
-            batteryDischarged.innerHTML = (battery.dischargingTime == "Infinity")? "Infinity" : parseInt(battery.dischargingTime / 60, 10);
+            // Set initial status
+            setStatus();
+
+            // Set events
+            battery.addEventListener("levelchange", setStatus, false);
+            battery.addEventListener("chargingchange", setStatus, false);
+            battery.addEventListener("chargingtimechange", setStatus, false);
+            battery.addEventListener("dischargingtimechange", setStatus, false);
+        } else {
+            throw new Error('Battery API not supported on your device/computer');
         }
-        // Set initial status
-        setStatus();
-    
+    };
 
-        // Set events
-        battery.addEventListener("levelchange", setStatus, false);
-        battery.addEventListener("chargingchange", setStatus, false);
-        battery.addEventListener("chargingtimechange", setStatus, false);
-        battery.addEventListener("dischargingtimechange", setStatus, false);
-    }
-    else {
-        batterySupported.innerHTML = "Battery API not supported on your device/computer";
-    } 
+    let noGood = function (error) {
+        batterySupported.innerHTML = error.message;
+    };
+
+    navigator.getBattery() //returns a promise
+        .then(success)
+        .catch(noGood);
 })();
